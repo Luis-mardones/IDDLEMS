c9c38fc8365f76daa9638f7316f87b704eb7d250
Luis-mardones <luis.mardones2017@twk.cl>
2018-05-28 12:10:25 -0400

    testagain

--- test/src/test/Main.java
+++ test/src/test/Main.java
@@ -1,47 +1,39 @@
 package test;
 
-
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		IMyData di;
+		List<IMyData> lstObjects = new ArrayList<IMyData>();
 		
 		
 		Product producto = new Product();
 		producto.setName("peras");
 		producto.setCodigo("00345");
+		lstObjects.add(producto);
 		
 		Customer cliente = new Customer();
 		cliente.setName("juan carlos godinez");
 		cliente.setRut("23.244.634-2");
+		lstObjects.add(cliente);
 		
 		Employee empleado = new Employee();
 		empleado.setName("pedro perez pereira");
 		cliente.setRut("23.244.634-2");
+		lstObjects.add(empleado);
 		
 		Car auto = new Car();
 		auto.setName("ferrari");
 		auto.setCode("12");
+		lstObjects.add(auto);
 		
-		di = producto;
-		System.out.println(di.getName());
-		di = cliente;
-		System.out.println(di.getName());
-		
-		
-		di = empleado;
-		System.out.println(di.getName());
-		di = auto;
-		System.out.println(di.getName());
-		
-		operation(cliente);
-	}
-	
-	private static void operation(Customer cus) {
-		cus.getName();
-		System.out.println(cus.getName());
+		for(IMyData cur : lstObjects) {
+			System.out.println(cur.getName());
+		}
 		
 	}
 
